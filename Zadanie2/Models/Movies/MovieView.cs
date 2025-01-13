5d117d7875615fe97c8be1a69679b8effc33c72f
BlueJack14 <bluejack1414@gmail.com>
2025-01-13 21:19:17 +0100

    sql querry added

---
+++ Zadanie2/Models/Movies/MovieView.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlTypes;
+
+namespace WebApplication1.Models.Movies;
+
+public class MovieView
+{
+    public string company_name;
+    public int movies_count;
+    public string company_budget;
+}
